015d0f5a6a9316d50985201d64bff11e259c727c
kkoetje <kkoetje@gmail.com>
2019-07-26 12:07:32 -0500

    Added if statement that defaults dy to 0.1 if no input is given

--- PIXMakeVBAR.m
+++ PIXMakeVBAR.m
@@ -1,4 +1,8 @@
-function [instID, lineID] = PIXMakeVBAR( x1, y1, x2, y2, z, dy, name );
+function [instID, lineID] = PIXMakeVBAR( x1, y1, x2, y2, z, name, dy );
+
+if (ischar(name)==1 || isstring(name)==1) && nargin == 6
+    dy = 0.1;
+end
 
 % function [instID] = PIXMakeVBAR( x1, y1, x2, y2, z, name );
 %
